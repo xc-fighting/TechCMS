ae9cd3019f6c9ac34a28d5854dc2a4be5201a74b
Chen Xu <cxu264@usc.edu>
2018-10-17 23:50:17 -0700

    Reorganize the index file and add includes folder to include the modules needed for display, display the category in the navigation part

---
+++ includes/db.php
@@ -0,0 +1,22 @@
+<?php
+
+
+$db['db_host']="localhost";
+$db['db_user']="root";
+$db['db_password']="";
+$db['db_name']="techcms";
+
+foreach($db as $key => $value ){
+
+   define(strtoupper($key),$value);
+
+}
+
+$conn = mysqli_connect(DB_HOST,DB_USER,DB_PASSWORD,DB_NAME);
+if( $conn ){
+
+ // echo "connected";
+
+}
+
+?>
---
+++ includes/navigation.php
@@ -0,0 +1,33 @@
+ <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+        <div class="container">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="#">Chen's Blog</a>
+            </div>
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav">
+                    
+                    <?php
+                           $query = "SELECT * FROM CATEGORY";
+                           $select_all_category = mysqli_query($conn,$query);
+                           while( $row = mysqli_fetch_assoc($select_all_category) ){
+
+                               $cat_title =  $row['cat_title'];
+                               echo "<li><a href='#'>{$cat_title}</a></li>";
+
+                           }
+                    ?>
+
+                </ul>
+            </div>
+            <!-- /.navbar-collapse -->
+        </div>
+        <!-- /.container -->
+    </nav>
